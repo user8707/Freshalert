093044b52848023f587e2d0db3ded0cd909dcd73
Lenneseb <162321732+Lenneseb@users.noreply.github.com>
2024-04-17 15:39:04 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -91,5 +91,5 @@ if __name__ == "__main__":
     
 st.text('Herzlich Willkommen bei FreshAlert. Deine App für deine Lebensmittel!')
 st.text("Füge links deine ersten Lebensmittel zu deinem Digitalen Kühlschrank hinzu. 
-Wir werden dich daran erinnen, es rechtzeitig zu benutzen und dir so helfen keine Lebensmittel mehr zu verschwenden. 
-#StopFoodwaste")
+st.text('Wir werden dich daran erinnen, es rechtzeitig zu benutzen und dir so helfen keine Lebensmittel mehr zu verschwenden.')
+st.text('#StopFoodwaste')
