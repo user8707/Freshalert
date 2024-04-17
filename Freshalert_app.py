3425c60cb0e1867c534a10e0df8ce3cd9c693c33
Lenneseb <162321732+Lenneseb@users.noreply.github.com>
2024-04-17 15:37:06 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -13,7 +13,9 @@ def is_user_logged_in():
     return True  # Für dieses Beispiel gehe ich davon aus, dass der Benutzer nicht eingeloggt ist
    
 st.text('Herzlich Willkommen bei FreshAlert. Deine App für deine Lebensmittel!')
-st.text('Füge links deine ersten Lebensmittel zu deinem Digitalen Kühlschrank hinzu. Wir werden dich daran erinnen, es rechtzeitig zu benutzen und dir so helfen keine Lebensmittel mehr zu verschwenden. #StopFoodwaste')
+st.text('Füge links deine ersten Lebensmittel zu deinem Digitalen Kühlschrank hinzu. 
+Wir werden dich daran erinnen, es rechtzeitig zu benutzen und dir so helfen keine Lebensmittel mehr zu verschwenden. 
+#StopFoodwaste')
 
 def show_login_page():
     st.title("Login")
@@ -91,5 +93,4 @@ if __name__ == "__main__":
 
     main()
     
-st.text('Herzlich Willkommen bei FreshAlert. Deine App für deine Lebensmittel!')
-st.text('Füge links deine ersten Lebensmittel zu deinem Digitalen Kühlschrank hinzu. Wir werden dich daran erinnen, es rechtzeitig zu benutzen und dir so helfen keine Lebensmittel mehr zu verschwenden. #StopFoodwaste')
+
