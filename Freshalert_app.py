90cd17983ddd16320af887e79c10d1abc46d668e
Lenneseb <162321732+Lenneseb@users.noreply.github.com>
2024-04-17 15:32:36 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -89,5 +89,6 @@ def show_registration_page():
 if __name__ == "__main__":
 
     main()
+    
 st.text('Herzlich Willkommen bei FreshAlert. Deine App für deine Lebensmittel!')
-st.text("Füge links deine ersten Lebensmittel zu deinem Digitalen Kühlschrank hinzu. Wir werden dich daran erinnen, es rechtzeitig zu benutzen und dir so helfen keine Lebensmittel mehr zu verschwenden. #StopFoodwaste")
+st.text('Füge links deine ersten Lebensmittel zu deinem Digitalen Kühlschrank hinzu. Wir werden dich daran erinnen, es rechtzeitig zu benutzen und dir so helfen keine Lebensmittel mehr zu verschwenden. #StopFoodwaste')
