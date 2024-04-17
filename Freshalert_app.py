b21fdaf742feb30c37bbd16ef50e55e839aedbbb
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-17 21:17:14 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
+from datetime import date
 from github_contents import GithubContents
+
+# Set constants
+DATA_FILE = "FreshAlert-Lebensmittel.csv"
+DATA_COLUMNS = ["Lebensmittel", "Kategorie", "Lagerort", "Ablaufdatum"]
 
 st.set_page_config(page_title="FreshAlert", page_icon="🗄️", layout="wide")
 
@@ -28,6 +33,50 @@ def show_login_page():
     if st.session_state.get("show_registration", False):
         with st.sidebar:
             show_registration_page()
+
+def init_github():
+    """Initialize the GithubContents object."""
+    if 'github' not in st.session_state:
+        st.session_state.github = GithubContents(
+            st.secrets["github"]["owner"],
+            st.secrets["github"]["repo"],
+            st.secrets["github"]["token"])
+
+def init_dataframe():
+    """Initialize or load the dataframe."""
+    if 'df' in st.session_state:
+        pass
+    elif st.session_state.github.file_exists(DATA_FILE):
+        st.session_state.df = st.session_state.github.read_df(DATA_FILE)
+    else:
+        st.session_state.df = pd.DataFrame(columns=DATA_COLUMNS)
+
+def add_entry_in_sidebar():
+    """Add a new entry to the DataFrame using pd.concat and calculate age."""
+    new_entry = {
+        DATA_COLUMNS[0]:  st.sidebar.text_input(DATA_COLUMNS[0]),  # LM
+        DATA_COLUMNS[1]:  st.sidebar.text_input(DATA_COLUMNS[1]),  # Kategorie
+        DATA_COLUMNS[2]:  st.sidebar.text_input(DATA_COLUMNS[2]),  # Lagerort
+        DATA_COLUMNS[4]:  st.sidebar.date_input(DATA_COLUMNS[4],
+                                                min_value=date(2024, 1, 1),
+                                                format="DD.MM.YYYY"),  # MHD
+    } 
+
+    # check wether all data is defined, otherwise show an error message
+    for key, value in new_entry.items():
+        if value == "":
+            st.sidebar.error(f"Bitte ergänze das Feld '{key}'")
+            return
+
+    if st.sidebar.button("Add"):
+        new_entry_df = pd.DataFrame([new_entry])
+        st.session_state.df = pd.concat([st.session_state.df, new_entry_df], ignore_index=True)
+
+        # Save the updated DataFrame to GitHub
+        name = new_entry[DATA_COLUMNS[0]]
+        msg = f"Add contact '{name}' to the file {DATA_FILE}"
+        st.session_state.github.write_df(DATA_FILE, st.session_state.df, msg)
+        
 def show_fresh_alert_page():
     st.title("FreshAlert")
     st.sidebar.title("")
