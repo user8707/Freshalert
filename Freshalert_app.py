cd7f6bf3dc864fd116756f1340e1f9ac35bd3e18
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-18 10:47:24 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -139,7 +139,8 @@ if __name__ == "__main__":
     main()
     
 st.subheader("Herzlich Willkommen bei FreshAlert. Deine App für deine Lebensmittel!"
-""Füge links deine ersten Lebensmittel zu deinem Digitalen Kühlschrank hinzu.""
-""Wir werden dich daran erinnen, es rechtzeitig zu benutzen und dir so helfen keine Lebensmittel mehr zu verschwenden.""
-"""#StopFoodwaste""")
+             
+"Füge links deine ersten Lebensmittel zu deinem Digitalen Kühlschrank hinzu."
+"Wir werden dich daran erinnen, es rechtzeitig zu benutzen und dir so helfen keine Lebensmittel mehr zu verschwenden."
+"#StopFoodwaste")
 
