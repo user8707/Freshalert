18ce2a0b9b2babb222ae587c7c7e277476594e1f
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-25 11:02:49 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -169,8 +169,7 @@ def add_food_to_fridge():
         st.dataframe(st.session_state.df_food)
     else:
         st.write("Der Kühlschrank ist leer.")
-
-save_data_to_database_food()  # Save the updated dataframe to GitHub
+        save_data_to_database_food()  # Save the updated dataframe to GitHub
 
 
 def save_data_to_database_food():
