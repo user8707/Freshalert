fd66e4349fe9f331258c9b8c6b6b4fd570f530d4
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-28 21:11:32 +0200

    Update and rename pages.py to login.py

---
+++ pages/login.py
@@ -0,0 +1,51 @@
+# pages.py
+import streamlit as st
+
+def show_login_page():
+    st.title("Welcome to FreshAlert ☺️, Let's start together with saving food") 
+    st.title("Login")
+    email = st.text_input("E-Mail", key="login_email")
+    password = st.text_input("Passwort", type="password", key="login_password")
+    if st.button("Login"):
+        login_successful = False
+        for index, row in st.session_state.df_login.iterrows():
+            if row["E-Mail"] == email and row["Passwort"] == password:
+                login_successful = True
+                break
+        if login_successful:
+            st.session_state.user_logged_in = True
+            st.success("Erfolgreich eingeloggt!")
+        else:
+            st.error("Ungültige E-Mail oder Passwort.")
+    if st.button("Registrieren", key="registration_button"):
+        st.session_state.show_registration = True
+    if st.session_state.get("show_registration", False):
+        show_registration_page()
+
+def show_registration_page():
+    st.title("Registrieren")
+           
+    new_entry = {
+        DATA_COLUMNS[0]: st.text_input(DATA_COLUMNS[0]), #Vorname
+        DATA_COLUMNS[1]: st.text_input(DATA_COLUMNS[1]), #Nachname
+        DATA_COLUMNS[2]: st.text_input(DATA_COLUMNS[2]), # E-Mail
+        DATA_COLUMNS[3]: st.text_input(DATA_COLUMNS[3], type="password"), #Passwort
+        DATA_COLUMNS[4]: st.text_input(DATA_COLUMNS[4], type="password"), #Passwort wiederholen
+    }
+
+    for key, value in new_entry.items():
+        if value == "":
+            st.error(f"Bitte ergänze das Feld '{key}'")
+            return
+
+    if st.button("Registrieren"):
+        if new_entry["Passwort"] == new_entry["Passwort wiederholen"]:
+            new_entry_df = pd.DataFrame([new_entry])
+            st.session_state.df_login = pd.concat([st.session_state.df_login, new_entry_df], ignore_index=True)
+            save_data_to_database_login()
+            st.success("Registrierung erfolgreich!")
+            st.session_state.show_registration = False  # Reset status
+        else:
+            st.error("Die Passwörter stimmen nicht überein.")
+
+
