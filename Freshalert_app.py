a268716f7428ae0a51e3f5cb628cf377a0ff49ff
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-28 22:54:55 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -15,7 +15,7 @@ DATA_COLUMNS_FOOD = ["Lebensmittel", "Kategorie", "Lagerort", "Ablaufdatum", "St
 image = Image.open('Logo_Freshalert.png')
 
 # Verkleinere das Bild
-small_image = image.resize((100, 10))
+small_image = image.resize((100, 1000))
 
 # Set page configuration
 st.set_page_config(
