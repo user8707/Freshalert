de85887bc7a93e07f8c51c77ce516dbf10b66c24
user8707 <161850907+user8707@users.noreply.github.com>
2024-04-28 19:23:46 +0200

    Create github_utils.py

---
+++ pages/github_utils.py
@@ -0,0 +1,12 @@
+# github_utils.py
+from github_contents import GithubContents
+import streamlit as st
+
+def init_github():
+    """Initialize the GithubContents object."""
+    if 'github' not in st.session_state:
+        st.session_state.github = GithubContents(
+            st.secrets["github"]["owner"],
+            st.secrets["github"]["repo"],
+            st.secrets["github"]["token"]
+        )
