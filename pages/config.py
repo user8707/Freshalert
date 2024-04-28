87b2d2620e73b0a0ee82ddfcbc625a4351dfe87f
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-28 21:09:18 +0200

    Update config.py

--- pages/config.py
+++ pages/config.py
@@ -3,3 +3,11 @@ DATA_FILE = "FreshAlert-Registration.csv"
 DATA_COLUMNS = ["Vorname", "Nachname", "E-Mail", "Passwort", "Passwort wiederholen"]
 DATA_FILE_FOOD = "Kühlschrankinhalt.csv"
 DATA_COLUMNS_FOOD = ["Lebensmittel", "Kategorie", "Lagerort", "Ablaufdatum", "Standort"]
+
+# Set page configuration
+st.set_page_config(
+    page_title="FreshAlert",
+    page_icon="🗄️",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
