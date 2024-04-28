acff596bbf17a8eaed49bdd95ac2dbaea5f1e42d
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-28 21:26:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ from pages.github_utils import init_github
 from pages.login import show_login_page, show_registration_page
 from pages.fridge import show_fresh_alert_page, show_mainpage, show_my_fridge_page, add_food_to_fridge, show_my_friends, show_settings
 
-st.set_page_config(
-    page_title="FreshAlert",
-    page_icon="🗄️",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
 
 def main():
     st.set_page_config(
