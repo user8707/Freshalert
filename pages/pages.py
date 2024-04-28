0d730fa68d4699168d9ee408ae72c84eebf3ea9d
user8707 <161850907+user8707@users.noreply.github.com>
2024-04-28 19:24:49 +0200

    Create pages.py

---
+++ pages/pages.py
@@ -0,0 +1,13 @@
+# pages.py
+import streamlit as st
+
+def show_login_page():
+    # Implementierung der Login-Seite
+
+def show_registration_page():
+    # Implementierung der Registrierungsseite
+
+def show_fresh_alert_page():
+    # Implementierung der Hauptseite
+
+# Weitere Funktionen für andere Seiten
