36669b288b09e2bd8e6600ac1c12845776c1f0ff
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-29 21:37:25 +0200

    Create constants.py

---
+++ konstante/constants.py
@@ -0,0 +1,9 @@
+# constants.py
+
+# Konstanten für die Benutzerregistrierung
+DATA_FILE = "FreshAlert-Registration.csv"
+DATA_COLUMNS = ["Vorname", "Nachname", "E-Mail", "Passwort", "Passwort wiederholen"]
+
+# Konstanten für den Kühlschrankinhalt
+DATA_FILE_FOOD = "Kühlschrankinhalt.csv"
+DATA_COLUMNS_FOOD = ["Lebensmittel", "Kategorie", "Lagerort", "Standort", "Ablaufdatum"]
