1ea639d504025f08067806fad061800ae6c1fc6d
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-29 21:29:46 +0200

    Delete pages directory

--- pages/__init__.py
+++
@@ -1,2 +0,0 @@
-from .fridge import show_mainpage, show_my_fridge_page, add_food_to_fridge, show_my_friends, show_settings
-
--- pages/config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-DATA_FILE = "FreshAlert-Registration.csv"
-DATA_COLUMNS = ["Vorname", "Nachname", "E-Mail", "Passwort", "Passwort wiederholen"]
-DATA_FILE_FOOD = "Kühlschrankinhalt.csv"
-DATA_COLUMNS_FOOD = ["Lebensmittel", "Kategorie", "Lagerort", "Ablaufdatum", "Standort"]
-
