7ca30e437dcb6e89a8ab92650f00d03cb3c1c710
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-30 13:59:36 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -25,6 +25,18 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
+def init_github():
+    """Initialize the GithubContents object and other session state variables."""
+    if 'github' not in st.session_state:
+        st.session_state.github = GithubContents(
+            st.secrets["github"]["owner"],
+            st.secrets["github"]["repo"],
+            st.secrets["github"]["token"]
+        )
+
+    # Initialize settings_enabled attribute
+    if 'settings_enabled' not in st.session_state:
+        st.session_state.settings_enabled = True
 
 def main():
     # Initialize GithubContents object and other session state variables
@@ -46,18 +58,7 @@ def main():
         # Show fresh alert page if user is logged in
         fresh_alert.show_fresh_alert_page()
 
-def init_github():
-    """Initialize the GithubContents object and other session state variables."""
-    if 'github' not in st.session_state:
-        st.session_state.github = GithubContents(
-            st.secrets["github"]["owner"],
-            st.secrets["github"]["repo"],
-            st.secrets["github"]["token"]
-        )
 
-    # Initialize settings_enabled attribute
-    if 'settings_enabled' not in st.session_state:
-        st.session_state.settings_enabled = True
 
 def init_dataframe_login():
     """Initialize or load the dataframe for user registration."""
