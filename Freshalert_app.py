48dc2151eab7732b11e2a7ded06a87441bdce79e
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-30 14:22:43 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -1,22 +1,8 @@
+# app.py
+
 import streamlit as st
-import pandas as pd
-from pages.github_contents import GithubContents
-from PIL import Image
 from pages import login, fresh_alert
-
-# Set constants for user registration
-DATA_FILE = "FreshAlert-Registration.csv"
-DATA_COLUMNS = ["Vorname", "Nachname", "E-Mail", "Passwort", "Passwort wiederholen"]
-
-# Set constants for fridge contents
-DATA_FILE_FOOD = "Kühlschrankinhalt.csv"
-DATA_COLUMNS_FOOD = ["Lebensmittel", "Kategorie", "Lagerort", "Standort", "Ablaufdatum"]
-
-# Load the image
-image = Image.open('images/Logo_Freshalert-Photoroom.png')
-
-# Resize the image
-small_image = image.resize((90, 105))
+from functions import init_github, init_dataframe_login, init_dataframe_food
 
 # Set page configuration
 st.set_page_config(
@@ -25,40 +11,19 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-def init_github():
-    """Initialize the GithubContents object and other session state variables."""
-    if 'github' not in st.session_state:
-        st.session_state.github = GithubContents(
-            st.secrets["github"]["owner"],
-            st.secrets["github"]["repo"],
-            st.secrets["github"]["token"]
-        )
-
-    # Initialize settings_enabled attribute
-    if 'settings_enabled' not in st.session_state:
-        st.session_state.settings_enabled = True
 
 def main():
-    # Initialize GithubContents object and other session state variables
     init_github()
-
-    # Initialize or load the dataframe for user registration
     init_dataframe_login()
-
-    # Initialize or load the dataframe for fridge contents
     init_dataframe_food()
-
     if 'user_logged_in' not in st.session_state:
         st.session_state.user_logged_in = False
 
     if not st.session_state.user_logged_in:
-        # Show login page if user is not logged in
         login.show_login_page()
     else:
-        # Show fresh alert page if user is logged in
         fresh_alert.show_fresh_alert_page()
-
-
 
 if __name__ == "__main__":
     main()
+
