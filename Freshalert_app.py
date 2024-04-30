c2c531ddd7b773d890f6841ba10c7106d4971d1c
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-30 13:55:27 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -1,9 +1,22 @@
 import streamlit as st
 import pandas as pd
-from pages import login, fresh_alert
 from pages.github_contents import GithubContents
-from PIL import Image 
+from PIL import Image
+from pages import login, fresh_alert
 
+# Set constants for user registration
+DATA_FILE = "FreshAlert-Registration.csv"
+DATA_COLUMNS = ["Vorname", "Nachname", "E-Mail", "Passwort", "Passwort wiederholen"]
+
+# Set constants for fridge contents
+DATA_FILE_FOOD = "Kühlschrankinhalt.csv"
+DATA_COLUMNS_FOOD = ["Lebensmittel", "Kategorie", "Lagerort", "Standort", "Ablaufdatum"]
+
+# Load the image
+image = Image.open('Logo_Freshalert-Photoroom.png')
+
+# Resize the image
+small_image = image.resize((90, 105))
 
 # Set page configuration
 st.set_page_config(
@@ -13,31 +26,46 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-if st.button("Home"):
-    st.switch_page("Freshalert_app.py")
-if st.button("Login"):
-    st.switch_page("pages/login.py")
-if st.button("Kühlschrank"):
-    st.switch_page("pages/fresh_alert.py")
-
-
-# Bildpfade
-LOGO_IMAGE_PATH = "images/Logo_Freshalert-Photoroom.png"
-SIDEBAR_IMAGE_PATH = "images/18-04-_2024_11-16-47-Photoroom.png"
-INFO_IMAGE_PATH = "images/Foodwaste1.png"
-
 def main():
-    login.init_github()
-    login.init_dataframe_login()
-    login.init_dataframe_food()
+    # Initialize GithubContents object and other session state variables
+    init_github()
+
+    # Initialize or load the dataframe for user registration
+    init_dataframe_login()
+
+    # Initialize or load the dataframe for fridge contents
+    init_dataframe_food()
+
     if 'user_logged_in' not in st.session_state:
         st.session_state.user_logged_in = False
 
     if not st.session_state.user_logged_in:
+        # Show login page if user is not logged in
         login.show_login_page()
     else:
+        # Show fresh alert page if user is logged in
         fresh_alert.show_fresh_alert_page()
+
+def init_github():
+    """Initialize the GithubContents object and other session state variables."""
+    if 'github' not in st.session_state:
+        st.session_state.github = GithubContents(
+            st.secrets["github"]["owner"],
+            st.secrets["github"]["repo"],
+            st.secrets["github"]["token"]
+        )
+
+    # Initialize settings_enabled attribute
+    if 'settings_enabled' not in st.session_state:
+        st.session_state.settings_enabled = True
+
+def init_dataframe_login():
+    """Initialize or load the dataframe for user registration."""
+    login.init_dataframe_login()
+
+def init_dataframe_food():
+    """Initialize or load the dataframe for fridge contents."""
+    fresh_alert.init_dataframe_food()
 
 if __name__ == "__main__":
     main()
-
