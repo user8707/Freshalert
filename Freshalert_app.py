3587767cc2926292dd46e718f645e7ffef5a5869
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-05-16 10:57:17 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -149,7 +149,7 @@ def show_fresh_alert_page():
     # Check which page to display
     if navigation == "Übersicht":
         show_mainpage()
-    elif navigation == "Meine Vorräte":
+    elif navigation == "Mein Kühlschrank":
         show_my_fridge_page()
     elif navigation == "Neues Lebensmittel hinzufügen":
         add_food_to_fridge()
