c529d85f1a4649d269b34de403e33da74fab9964
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-05-18 22:49:18 +0200

    Update Freshalert_app.py

--- Freshalert_app.py
+++ Freshalert_app.py
@@ -202,7 +202,7 @@ def show_mainpage():
         user_first_name = user_info.get("Vorname")
         
         # Willkommensnachricht mit dem Namen des Benutzers anzeigen
-        st.subheader(f"Herzlich Willkommen {user_first_name}, bei FreshAlert)
+        st.subheader("Herzlich Willkommen {user_first_name}, bei FreshAlert)
         st.subheader(Deine App für deine Lebensmittel!")                      
     else:
         st.subheader("Herzlich Willkommen bei FreshAlert. Deine App für deine Lebensmittel!")  # Falls Vorname nicht verfügbar ist
